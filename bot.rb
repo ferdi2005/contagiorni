c70ec67b7152a3c4a5792ccb3234f2fffa2955e2
ferdi2005 <ferdi.traversa@gmail.com>
2020-12-17 09:26:28 +0100

    Aggiunta messaggio da inviare all'arrivo del giorno prefissato

--- bot.rb
+++ bot.rb
@@ -14,18 +14,25 @@ if !File.exist? "#{__dir__}/.config"
     puts "Inserisci chat_id del gruppo:"
     print '> '
     chat_id = gets.chomp
+    puts "Inserisci il messaggio da inviare all'arrivo della data dell'evento:"
+    print '> '
+    giorno = gets.chomp
     File.open("#{__dir__}/.config", "w") do |file| 
       file.puts token
       file.puts date
       file.puts event
       file.puts chat_id
+      file.puts giorno
     end
   end  
 userdata = File.open("#{__dir__}/.config", "r").to_a
 
 userdata.map { |e| e.strip! }
+
 Telegram::Bot::Client.run(userdata[0]) do |bot|
     if Date.today < Date.parse(userdata[1])
         bot.api.send_message(chat_id: userdata[3], text: "Mancano #{(Date.parse(userdata[1]) - Date.today).to_i} giorni #{userdata[2]}")
+    elsif Date.today == Date.parse(userdata[1])
+      bot.api.send_message(chat_id: userdata[3], text: "#{userdata[4]}")
     end
 end
